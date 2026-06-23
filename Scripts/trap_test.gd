c8095c8d8e11183b87b06b15fc1d09749d51dcc9
Xinyao <chiusinsin2698@gmail.com>
2026-06-23 16:45:04 +0800

    feat: electric arc

--- Scripts/trap_test.gd
+++ Scripts/trap_test.gd
@@ -3,51 +3,30 @@ extends Node2D
 
 func _input(event: InputEvent):
 	if event is InputEventKey and event.pressed:
-		var bounds = Global.stage.stage_bounds
 		match event.keycode:
 			KEY_H:
 				Global.player_hit.emit(3)
 			KEY_1:
-				Trap1Mine.initialize(
-					(
-						bounds.position
-						+ Vector2(randf_range(0, bounds.size.x), randf_range(0, bounds.size.y))
-					)
-				)
+				Trap1Mine.initialize(_random_pos())
 			KEY_2:
 				Trap2ElectricRing.initialize(randf_range(1.0, 2.0), randf_range(75, 150))
 			KEY_3:
 				Trap3TracingBullet.initialize(Vector2(100, 0), Vector2(0, -100), 200)
 			KEY_4:
-				Trap4Conveyor.initialize(
-					(
-						bounds.position
-						+ Vector2(randf_range(0, bounds.size.x), randf_range(0, bounds.size.y))
-					),
-					Vector2(1, 0)
-				)
+				Trap4Conveyor.initialize(_random_pos(), Vector2(1, 0))
 			KEY_5:
-				Trap5IceFloor.initialize(
-					(
-						bounds.position
-						+ Vector2(randf_range(0, bounds.size.x), randf_range(0, bounds.size.y))
-					)
-				)
+				Trap5IceFloor.initialize(_random_pos())
 			KEY_7:
 				Trap7SpreadingRipples.initialize(Vector2(300, 300), 150.0)
+			KEY_8:
+				Trap8ElectricArc.initialize(_random_pos(), _random_pos())
 			KEY_9:
-				Trap9Mortar.initialize(
-					(
-						bounds.position
-						+ Vector2(randf_range(0, bounds.size.x), randf_range(0, bounds.size.y))
-					),
-					(
-						bounds.position
-						+ Vector2(randf_range(0, bounds.size.x), randf_range(0, bounds.size.y))
-					),
-					2.0
-				)
+				Trap9Mortar.initialize(_random_pos(), _random_pos(), 2.0)
 			KEY_0:
 				Trap10Shotgun.initialize(
 					Vector2(-250, 100), Vector2(1, 0.2), Vector2(1, 0), Vector2(1, -0.2)
 				)
+
+func _random_pos() -> Vector2:
+	var bounds = Global.stage.stage_bounds
+	return bounds.position + Vector2(randf_range(0, bounds.size.x), randf_range(0, bounds.size.y))
--- Scripts/traps/trap8-electric_arc.gd
+++ Scripts/traps/trap8-electric_arc.gd
@@ -1,42 +1,58 @@
+class_name Trap8ElectricArc
 extends Node2D
-const ZERO_VECTOR = Vector2(0, 0)
-var points_standard_scale: Vector2 = Vector2(0.15, 0.15)
-var points_revolution_speed: float = 8
-var arc_standard_width: float = 5.0
-var scaling_rate: float = 10
+
+@export var points_radius: int = 10
+@export var points_default_scale: Vector2 = Vector2(0.15, 0.15)
+@export var points_revolution_speed: float = 8
+@export var arc_width: float = 5.0
+@export var scaling_rate: float = 10
+@export var player_radius: float = 5
+
 var points_scale: Vector2
 var points_assigned_scale: Vector2
 var arc_assigned_width: float
+var arc_on: bool
+var _data = Global.trap_data["trap8-electric_arc"]
+
+@onready var player: CharacterBody2D = $"../Player"
 @onready var start_point: Node2D = $start_point
 @onready var end_point: Node2D = $end_point
 @onready var start_point_sprite: Sprite2D = $start_point/sprite
 @onready var end_point_sprite: Sprite2D = $end_point/sprite
 @onready var arc: Line2D = $arc
 
+static func initialize(start_pos: Vector2, end_pos: Vector2) -> Trap8ElectricArc:
+	var trap := preload("res://Scenes/traps/trap8-electric_arc.tscn").instantiate()
+	Global.stage.add_child(trap)
+	trap.spawn(start_pos, end_pos, 2.0, 100)
+	return trap
+
 
 func _ready():
+	arc_on = false
+	arc.position = Vector2.ZERO
 	_build_arc()
 	_visible_set(0)
-	_spawn(Vector2(_randx(), _randy()), Vector2(_randx(), _randy()), 2.0, 3.0)
 
 
 func _build_arc() -> void:
-	arc.add_point(ZERO_VECTOR, 0)
-	arc.add_point(ZERO_VECTOR, 1)
+	arc.add_point(Vector2.ZERO, 0)
+	arc.add_point(Vector2.ZERO, 1)
 	arc.width = 4.0
 
 
 func _randx() -> float:
-	return randf_range(0, 1152)
+	return randf_range(326, 826)
 
 
 func _randy() -> float:
-	return randf_range(0, 648)
+	return randf_range(74, 574)
 
 
 func _physics_process(delta: float) -> void:
 	_points_rotate(delta)
 	_scaling(delta)
+	_detect_player()
 
 
 func _points_rotate(delta: float):
@@ -51,26 +67,28 @@ func _scaling(delta: float) -> void:
 	arc.width += (arc_assigned_width - arc.width) * scaling_rate * delta
 
 
-func _spawn(
+func spawn(
 	start_position: Vector2, end_position: Vector2, delay_time: float, duration_time: float
 ) -> void:
-	start_point.global_position = start_position
-	end_point.global_position = end_position
+	start_point.position = start_position
+	end_point.position = end_position
 	arc.set_point_position(0, start_position)
 	arc.set_point_position(1, end_position)
-	points_scale = ZERO_VECTOR
-	points_assigned_scale = points_standard_scale
+	points_scale = Vector2.ZERO
+	points_assigned_scale = points_default_scale
 	arc.width = 0
-	arc_assigned_width = arc_standard_width
+	arc_assigned_width = arc_width
 
 	_visible_set(1)
 	await get_tree().create_timer(delay_time).timeout
 
 	_visible_set(2)
+	arc_on = true
 	await get_tree().create_timer(duration_time).timeout
 
-	points_assigned_scale = ZERO_VECTOR
+	points_assigned_scale = Vector2.ZERO
 	arc_assigned_width = 0
+	arc_on = false
 	await get_tree().create_timer(0.5).timeout
 	_visible_set(0)
 
@@ -88,3 +106,18 @@ func _visible_set(mode: int) -> void:
 		start_point_sprite.modulate = Color(1.0, 1.0, 1.0, 1.0)
 		end_point_sprite.modulate = Color(1.0, 1.0, 1.0, 1.0)
 		arc.modulate = Color(1.0, 1.0, 1.0, 1.0)
+
+
+func _detect_player() -> void:
+	if not arc_on:
+		return
+	var vector_start_to_end = end_point.global_position - start_point.global_position
+	var vector_start_to_player = player.global_position - start_point.global_position
+	var vector_end_to_player = player.global_position - end_point.global_position
+	var dist = abs((vector_start_to_end.cross(vector_start_to_player)) / vector_start_to_end.length())
+	if (
+		(dist <= player_radius + arc_width and vector_start_to_player.dot(vector_start_to_end) >= 0 and vector_end_to_player.dot(-vector_start_to_end) >= 0)
+		or vector_start_to_player.length() <= player_radius + points_radius
+		or vector_end_to_player.length() <= player_radius + points_radius
+	):
+		Global.player_hit.emit(5)
