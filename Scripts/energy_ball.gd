c54aabe9c59089c73beb79e969a22579a9e8ddf9
CJN <b14902017@csie.ntu.edu.tw>
2026-06-23 09:40:17 +0800

    feat: make the energy ball a coconut
    - The energy ball sprite is now a coconut.

--- Scripts/energy_ball.gd
+++ Scripts/energy_ball.gd
@@ -4,8 +4,13 @@ extends Area2D
 @export var min_spawn_distance_from_player := 48.0
 @export var max_spawn_attempts := 100
 @export var player_node: Node2D
+@export var coconut_flicker_speed := 0.1
+@export var coconut_rotate_speed := 0.1
+@export var coconut_rotate_amplitude := 0.1
 
 var rng := RandomNumberGenerator.new()
+
+@onready var coconut_sprite = $Coconut
 
 
 func _ready() -> void:
@@ -45,3 +50,11 @@ func _get_random_spawn_position() -> Vector2:
 		attempts += 1
 
 	return energy_ball_spawn_bounds.get_center()
+
+
+func _physics_process(_delta: float) -> void:
+	var rotate = coconut_rotate_amplitude * sin(Engine.get_frames_drawn() * coconut_rotate_speed)
+	coconut_sprite.rotation = rotate
+	var b = abs(sin(Engine.get_frames_drawn() * coconut_flicker_speed))
+	var g = 0.75 + sin(Engine.get_frames_drawn() * (coconut_flicker_speed + 0.05)) * 0.25
+	coconut_sprite.material.set_shader_parameter("outline_color", Vector4(1, g, b, 1))
