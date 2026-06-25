ad63af5920855e8e2ef6e7f473f1719bb6db42a2
prairie2022 <anthonycho0912@gmail.com>
2026-06-25 11:32:27 +0800

    feat: add Trap 6 to `trap_test` and `trap.json`

--- Scripts/trap_test.gd
+++ Scripts/trap_test.gd
@@ -18,7 +18,7 @@ func _input(event: InputEvent):
 			KEY_5:
 				Trap5IceFloor.initialize(_random_pos())
 			KEY_6:
-				var angle = randf_range(0, PI * 26)
+				var angle = randf_range(0, PI * 2)
 				Trap6Scanline.initialize(Vector2(cos(angle), sin(angle)), 100)
 			KEY_7:
 				Trap7SpreadingRipples.initialize(Vector2(300, 300), 150.0)
