968734a1e9cb1de84a8ee3702e7e13b8a08844b8
cmj0415 <cmj51406002@gmail.com>
2026-06-25 11:05:56 +0800

    fix: bullet collision layer; instantiation; speed clamp

--- Scripts/traps/trap3-tracing_bullet.gd
+++ Scripts/traps/trap3-tracing_bullet.gd
@@ -3,54 +3,43 @@ extends CharacterBody2D
 
 const PLAYER_COLLISION_LAYER := 1
 const WALL_COLLISION_LAYER := 2
+const TRAP_COLLISION_LAYER := 4
 
 @export var max_turn_rate := 2.0
+@export var max_speed := 300.0
+@export var min_speed := 100.0
 
 var target: Node2D = null
 var speed := 0.0
-var active := false
 var tracing := true
 var _data: Dictionary = Global.trap_data["trap3-tracing_bullet"]
 
 
 static func initialize(pos: Vector2, dir: Vector2, speed: float) -> Trap3TracingBullet:
 	var trap := preload("res://Scenes/traps/trap3-tracing_bullet.tscn").instantiate()
+	trap.position = pos
+	trap.rotation = dir.angle()
+	trap.speed = speed
+	trap.target = Global.game_manager.player
 	Global.stage.add_child(trap)
-	trap.activate(pos, dir, speed, Global.game_manager.player)
 	return trap
 
 
 func _ready() -> void:
+	speed = clamp(speed, min_speed, max_speed)
+	collision_layer = TRAP_COLLISION_LAYER
 	collision_mask = PLAYER_COLLISION_LAYER | WALL_COLLISION_LAYER
-	set_physics_process(false)
-	visible = false
-
-
-func activate(
-	spawn_position: Vector2, initial_direction: Vector2, new_speed: float, new_target: Node2D
-):
-	position = spawn_position
-	rotation = initial_direction.angle()
-
-	speed = new_speed
-	target = new_target
-
-	active = true
 	tracing = true
-	visible = true
 	set_physics_process(true)
 
 
-func deactivate():
+func _destroy() -> void:
 	queue_free()
 
 
 func _physics_process(delta):
-	if not active:
-		return
-
 	if target == null:
-		deactivate()
+		_destroy()
 		return
 
 	if tracing:
@@ -68,9 +57,9 @@ func _physics_process(delta):
 
 		if collider == target:
 			Global.player_hit.emit(67)
-			deactivate()
+			_destroy()
 		elif (collider.collision_layer & WALL_COLLISION_LAYER) != 0:
-			deactivate()
+			_destroy()
 
 
 func turn_toward_target(delta):
