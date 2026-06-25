16dfafcdc86a871ecd152ed5e86eae170e5c7901
PsychoPinkQ <PsychoPinkQ@gmail.com>
2026-06-25 22:47:29 +0800

    fix: modified to fit trap format

--- Scripts/trap_test.gd
+++ Scripts/trap_test.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 func _input(event: InputEvent):
 	if event is InputEventKey and event.pressed:
+		print("meow")
 		match event.keycode:
 			KEY_H:
 				Global.player_hit.emit(3)
@@ -17,7 +18,7 @@ func _input(event: InputEvent):
 			KEY_5:
 				Trap5IceFloor.initialize(_random_pos())
 			KEY_6:
-				var angle = randf_range(0, PI * 2)
+				var angle = randf_range(0, PI * 26)
 				Trap6Scanline.initialize(Vector2(cos(angle), sin(angle)), 100)
 			KEY_7:
 				Trap7SpreadingRipples.initialize(Vector2(300, 300), 150.0)
--- Scripts/traps/trap6-scanline.gd
+++ Scripts/traps/trap6-scanline.gd
@@ -1,20 +1,24 @@
 class_name Trap6Scanline
 extends Area2D
 
-@export var line_dir = PI / 180 * 15
+@export var line_dir = Vector2(0, 0)
 var line_alive = 1
-var velocity = Vector2(-sin(line_dir), cos(line_dir)) * 2.5
-var line_pos = Vector2(0, 0) - velocity * 500
+var velocity = Vector2(0, 0)
+var speed = 5
+var line_pos = Vector2(0, 0)
 
 @onready var visual_line = $ColorRect/Sprite2D
 @onready var collision_shape = $CollisionShape2D
 
 
-static func initialize(dir: Vector2, speed: float) -> Trap6Scanline:
-	var trap := preload("res://Scenes/traps/trap6-scanline.tscn").instantiate()
+static func initialize(dir0: Vector2, speed0: float) -> Trap6Scanline:
+	var trap := preload("res://Scenes/traps/trap6-scanline.tscn").instantiate() as Trap6Scanline
+	trap.line_dir = dir0.normalized()
+	trap.speed = speed0
+	trap.velocity = dir0.rotated(-PI / 2) * trap.speed
+	trap.line_pos = Vector2(0, 0) - dir0.rotated(-PI / 2) * 500
+	trap.visible = true
 	Global.stage.add_child(trap)
-	# TODO: Set initial position and direction
-	velocity = dir.normalized() * speed
 	return trap
 
 
@@ -24,8 +28,8 @@ func _ready() -> void:
 	visual_line.visible = true
 	visual_line.position = Vector2(500, 500) + line_pos
 	collision_shape.position = line_pos
-	collision_shape.rotation = line_dir
-	visual_line.rotation = line_dir
+	visual_line.rotation = line_dir.angle()
+	collision_shape.rotation = line_dir.angle()
 	body_entered.connect(_on_body_entered)
 
 
@@ -35,12 +39,12 @@ func _process(_delta: float) -> void:
 
 
 func _on_body_entered(_body: Node2D) -> void:
-	print("player touched trap lines")
+	print("player touched scan lines")
 	Global.player_hit.emit(randi_range(0, 10))
 	#line_alive = false
 	#visible = false
 
 
-func _physics_process(_delta):
-	visual_line.position += velocity
-	collision_shape.position += velocity
+func _physics_process(delta):
+	visual_line.position += velocity * delta
+	collision_shape.position += velocity * delta
