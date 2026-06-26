098b1c73d09b1ede05d0b789261a199b6ff9ccae
CYhuang314 <antony.huang0301@gmail.com>
2026-06-26 15:57:32 +0800

    fix: remove external velocity after trap 4 disappeared

--- Scripts/traps/trap4-conveyor.gd
+++ Scripts/traps/trap4-conveyor.gd
@@ -4,6 +4,7 @@ extends Area2D
 @export var speed: float = 100
 @export var lifetime: float = 5.0
 var direction: Vector2
+var _is_disappearing: bool = false
 
 
 static func initialize(pos: Vector2, dir: Vector2) -> Trap4Conveyor:
@@ -24,11 +25,8 @@ func _destroy_trap() -> void:
 	if not is_inside_tree():
 		return
 
-	for body in get_overlapping_bodies():
-		if body == Global.game_manager.player and body is CharacterBody2D:
-			body.external_velocity -= speed * direction
-			print("trap timeout: remove player conveyor force")
-			queue_free()
+	_is_disappearing = true
+	queue_free()
 
 
 func _on_body_entered(body: Node2D) -> void:
@@ -40,4 +38,7 @@ func _on_body_entered(body: Node2D) -> void:
 func _on_body_exited(body: Node2D) -> void:
 	if body == Global.game_manager.player and body is CharacterBody2D:
 		body.external_velocity -= speed * direction
-		print("玩家離開了履帶地塊")
+		if _is_disappearing:
+			print("陷阱超時消失，強制解除玩家的履帶效果")
+		else:
+			print("玩家離開了履帶地塊")
