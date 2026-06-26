611bc2fd08e86c02fc4f0d4625b164f3df7ed592
Peyton <pppeyton.8320@gmail.com>
2026-06-26 16:35:38 +0800

    feat: resprite trap 9

--- Scripts/traps/trap9-mortar.gd
+++ Scripts/traps/trap9-mortar.gd
@@ -5,7 +5,7 @@ extends Node2D
 @export var gravity: float = 200.0
 @export var test_player: CharacterBody2D
 @export var explosion_max_radius: float = 100.0
-@export var explosion_expand_speed: float = 400.0
+@export var stay_time: float = 0.25
 @export var damage: int = 20
 
 var player: CharacterBody2D
@@ -23,7 +23,7 @@ var explosion_radius: float = 0.0
 
 @onready var shell: Sprite2D = $Shell
 @onready var shadow: Sprite2D = $Shadow
-@onready var explosion: AnimatedSprite2D = $Explosion
+@onready var explosion: Sprite2D = $Explosion
 @onready var explosion_area: Area2D = $ExplosionArea
 @onready var explosion_shape: CollisionShape2D = $ExplosionArea/CollisionShape2D
 
@@ -36,9 +36,6 @@ static func initialize(start_pos: Vector2, end_pos: Vector2, air_time: float) ->
 
 
 func _ready() -> void:
-	explosion.visible = false
-	explosion.animation_finished.connect(_on_explosion_finished)
-
 	shell.z_index = 1
 	shadow.z_index = 0
 
@@ -46,27 +43,21 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if flying:
 		elapsed += delta
-
 		velocity.y += gravity * delta
-
 		shell.position += velocity * delta
-
 		shadow.position = Vector2(shell.position.x, end_pos.y)
-
 		if elapsed >= air_time:
 			explode()
 
 	if exploding:
-		explosion_radius += explosion_expand_speed * delta
-
-		if explosion_radius >= explosion_max_radius:
-			queue_free()
-
+		elapsed += delta
 		for body in explosion_area.get_overlapping_bodies():
 			if body == Global.game_manager.player:
 				Global.player_hit.emit(damage)
-
-		queue_redraw()
+		if elapsed >= air_time + stay_time:
+			exploding = false
+			explosion_area.monitoring = false
+			_fade_out()
 
 
 func activate(
@@ -84,16 +75,15 @@ func activate(
 	exploding = false
 	explosion_area.monitoring = false
 
+	explosion.visible = false
+	var effect = explosion.get_node("GPUParticles2D") as GPUParticles2D
+	effect.lifetime = stay_time
+
 	shell.position = start_pos
 	shadow.position = start_pos
 	velocity.x = (end_pos.x - start_pos.x) / air_time
 
 	velocity.y = (end_pos.y - start_pos.y - 0.5 * gravity * air_time * air_time) / air_time
-
-
-func _on_explosion_finished():
-	explosion_area.monitoring = false
-	queue_free()
 
 
 func explode() -> void:
@@ -106,14 +96,19 @@ func explode() -> void:
 	circle.radius = explosion_max_radius
 	explosion_area.monitoring = true
 
-	explosion.visible = true
+	var effect = explosion.get_node("GPUParticles2D") as GPUParticles2D
+	if not effect.finished.is_connected(effect.queue_free):
+		effect.finished.connect(effect.queue_free)
+	effect.emitting = true
 	exploding = true
 	explosion_radius = 0.0
+	explosion.visible = true
 
 	position = end_pos
-	# explosion.play()
 
 
-func _draw():
-	if exploding:
-		draw_circle(Vector2.ZERO, explosion_radius, Color(1, 0.5, 0, 0.3))
+func _fade_out():
+	var tween = create_tween()
+	tween.tween_property(self, "modulate", Color(1, 1, 1, 0), stay_time / 2.5)
+	await tween.finished
+	queue_free()
