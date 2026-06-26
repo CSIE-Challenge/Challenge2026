dd131a1bce3526a21c88956fe68a58af9dd38360
prairie2022 <anthonycho0912@gmail.com>
2026-06-26 17:21:10 +0800

    chore: reorganize variables

--- Scripts/traps/trap6-scanline.gd
+++ Scripts/traps/trap6-scanline.gd
@@ -1,22 +1,26 @@
 class_name Trap6Scanline
 extends Area2D
 
-@export var line_dir = Vector2(0, 0)
-var line_alive = 1
-var velocity = Vector2(0, 0)
-var speed = 5
-var line_pos = Vector2(0, 0)
+const LINE_SHIFT := 500
+@export var damage: int = 5
+
+var line_dir := Vector2(0, 0)
+var line_alive := 1
+var velocity := Vector2(0, 0)
+var speed := 5
+var line_pos := Vector2(0, 0)
+var _data: Dictionary = Global.trap_data["trap6-scanline"]
 
 @onready var visual_line = $ColorRect/Sprite2D
 @onready var collision_shape = $CollisionShape2D
 
 
 static func initialize(dir0: Vector2, speed0: float) -> Trap6Scanline:
-	var trap := preload("res://Scenes/traps/trap6-scanline.tscn").instantiate() as Trap6Scanline
+	var trap := preload("res://Scenes/traps/trap6-scanline.tscn").instantiate()
 	trap.line_dir = dir0.normalized()
 	trap.speed = speed0
 	trap.velocity = dir0.rotated(-PI / 2) * trap.speed
-	trap.line_pos = Vector2(0, 0) - dir0.rotated(-PI / 2) * 500
+	trap.line_pos = Vector2(0, 0) - dir0.rotated(-PI / 2) * LINE_SHIFT
 	trap.visible = true
 	Global.stage.add_child(trap)
 	return trap
@@ -26,7 +30,7 @@ static func initialize(dir0: Vector2, speed0: float) -> Trap6Scanline:
 func _ready() -> void:
 	visible = true
 	visual_line.visible = true
-	visual_line.position = Vector2(500, 500) + line_pos
+	visual_line.position = Vector2(LINE_SHIFT, LINE_SHIFT) + line_pos
 	collision_shape.position = line_pos
 	visual_line.rotation = line_dir.angle()
 	collision_shape.rotation = line_dir.angle()
@@ -34,17 +38,11 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _process(delta: float) -> void:
+	visual_line.position += velocity * delta
+	collision_shape.position += velocity * delta
 
 
 func _on_body_entered(_body: Node2D) -> void:
 	print("player touched scan lines")
-	Global.player_hit.emit(randi_range(0, 10))
-	#line_alive = false
-	#visible = false
-
-
-func _physics_process(delta):
-	visual_line.position += velocity * delta
-	collision_shape.position += velocity * delta
+	Global.player_hit.emit(damage)
