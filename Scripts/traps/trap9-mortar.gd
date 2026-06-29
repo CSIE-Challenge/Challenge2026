fa1145780701fced908460c698c4100bde11df4c
CJN <b14902017@csie.ntu.edu.tw>
2026-06-29 10:54:54 +0800

    chore: adjust object layers
    - Adjusted object layers.
    - Also adjusted mortar trap's visual effect.

--- Scripts/traps/trap9-mortar.gd
+++ Scripts/traps/trap9-mortar.gd
@@ -7,6 +7,8 @@ extends Node2D
 @export var explosion_max_radius: float = 100.0
 @export var stay_time: float = 0.25
 @export var damage: int = 20
+@export var shell_rotate_speed: float = 20.0
+@export var shadow_flicker_frequency: float = 1
 
 var player: CharacterBody2D
 var start_pos: Vector2
@@ -20,9 +22,12 @@ var flying: bool = false
 
 var exploding: bool = false
 var explosion_radius: float = 0.0
+var shell_vertical_speed: float = 0.0
+var my_shell_rotate_speed: float
 
-@onready var shell: Sprite2D = $Shell
-@onready var shadow: Sprite2D = $Shadow
+@onready var shell: Sprite2D = $ShellShadow/Shell
+@onready var shell_shadow: Node2D = $ShellShadow
+@onready var shadow: Sprite2D = $ShellShadow/Shadow
 @onready var explosion: Sprite2D = $Explosion
 @onready var explosion_area: Area2D = $ExplosionArea
 @onready var explosion_shape: CollisionShape2D = $ExplosionArea/CollisionShape2D
@@ -40,12 +45,15 @@ func _ready() -> void:
 	shadow.z_index = 0
 
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if flying:
 		elapsed += delta
-		velocity.y += gravity * delta
-		shell.position += velocity * delta
-		shadow.position = Vector2(shell.position.x, end_pos.y)
+		shell_shadow.position += velocity * delta
+		shell.position.y += shell_vertical_speed * delta
+		shell_vertical_speed += gravity * delta
+		shell.rotation += my_shell_rotate_speed * delta
+		shadow.rotation = shell.rotation
+		shadow.self_modulate.r = 0.5 + 0.5 * sin(elapsed * shadow_flicker_frequency)
 		if elapsed >= air_time:
 			explode()
 
@@ -67,7 +75,6 @@ func activate(
 ) -> void:
 	start_pos = initial_position
 	end_pos = final_position
-
 	air_time = flight_time
 
 	elapsed = 0.0
@@ -79,11 +86,12 @@ func activate(
 	var effect = explosion.get_node("GPUParticles2D") as GPUParticles2D
 	effect.lifetime = stay_time
 
-	shell.position = start_pos
-	shadow.position = start_pos
+	shell.position = Vector2.ZERO
+	shell_shadow.position = start_pos
 	velocity.x = (end_pos.x - start_pos.x) / air_time
-
-	velocity.y = (end_pos.y - start_pos.y - 0.5 * gravity * air_time * air_time) / air_time
+	velocity.y = (end_pos.y - start_pos.y) / air_time
+	shell_vertical_speed = gravity * air_time / -2
+	my_shell_rotate_speed = shell_rotate_speed * randf_range(0.7, 1.3) * sign(randf_range(-1, 1))
 
 
 func explode() -> void:
@@ -103,6 +111,7 @@ func explode() -> void:
 	exploding = true
 	explosion_radius = 0.0
 	explosion.visible = true
+	explosion.rotation = randf_range(0, TAU)
 
 	position = end_pos
 
