969cddd46360b2fa21747c7ec56cade15d87ffe8
CJN <b14902017@csie.ntu.edu.tw>
2026-06-30 11:57:32 +0800

    feat: changed scanline sprite
    - Changed scanline sprite.
    - Scanlines will now oscillate.
    - Restricted scanline direction to 4.

--- Scripts/traps/trap6-scanline.gd
+++ Scripts/traps/trap6-scanline.gd
@@ -3,47 +3,71 @@ extends Area2D
 
 const LINE_SHIFT := 500  # 圖片中心和 Area2D 的中心的距離是 (500, 500)
 
+@export var oscillate_frequency: float = 0.5
+@export var oscillate_amplitude: float = 20
+
 var cooldown_times = TrapData.new().data["trap6-scanline"]["cooldown_times"]
 var damage = TrapData.new().data["trap6-scanline"]["damage"]
 var energy_costs = TrapData.new().data["trap6-scanline"]["energy_costs"]
 var speed_lower_bound = TrapData.new().data["trap6-scanline"]["speed_lower_bound"]
 var speed_upper_bound = TrapData.new().data["trap6-scanline"]["speed_upper_bound"]
 
-var line_dir: Vector2
-var velocity: Vector2
-var speed: int
-var line_pos: Vector2
+var line_dir := Vector2(0, 0)
+var line_alive := 1
+var velocity := Vector2(0, 0)
+var speed := 5.0
+var line_pos := Vector2(0, 0)
+var time := 0.0
 
-@onready var visual_line = $ColorRect/Sprite2D
-@onready var collision_shape = $CollisionShape2D
+@onready var visual_line = $Hulas
 
 
 static func initialize(dir0: Vector2, speed0: float) -> Trap6Scanline:
 	var trap := preload("res://Scenes/traps/trap6-scanline.tscn").instantiate()
-	trap.line_dir = dir0.normalized()
-	trap.speed = clamp(speed0, trap.speed_lower_bound, trap.speed_upper_bound)
-	trap.velocity = dir0.rotated(-PI / 2) * trap.speed
-	trap.line_pos = Vector2(0, 0) - dir0.rotated(-PI / 2) * LINE_SHIFT
-	trap.visible = true
+	trap.spawn(dir0, speed0)
 	Global.stage.add_child(trap)
 	return trap
+
+
+func spawn(dir0: Vector2, speed0: float):
+	line_dir = _clamp_to_four_directions(dir0).normalized()
+	print(line_dir)
+	speed = speed0
+	velocity = line_dir * speed
+	position = -line_dir * LINE_SHIFT
+	visible = true
+
+
+func _clamp_to_four_directions(dir: Vector2) -> Vector2:
+	if abs(dir.x) > abs(dir.y):
+		return Vector2(dir.x, 0)
+	return Vector2(0, dir.y)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	visible = true
 	visual_line.visible = true
-	visual_line.position = Vector2(LINE_SHIFT, LINE_SHIFT) + line_pos
-	collision_shape.position = line_pos
-	visual_line.rotation = line_dir.angle()
-	collision_shape.rotation = line_dir.angle()
 	body_entered.connect(_on_body_entered)
+	visual_line.modulate = Color.WHITE * randf_range(0.9, 1)
+	visual_line.modulate.a = 1
+	if abs(line_dir.x) > 0.1:
+		rotation = PI / 2
+		var hulas = visual_line.get_children()
+		for hula in hulas:
+			hula.rotation = -PI / 2
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	visual_line.position += velocity * delta
-	collision_shape.position += velocity * delta
+func _physics_process(delta: float) -> void:
+	position += velocity * delta
+	time += delta
+	visual_line.position.x = sin(time * TAU * oscillate_frequency) * oscillate_amplitude
+	visual_line.scale.x = sign(visual_line.position.x)
+	if abs(line_dir.x) > 0.1:
+		if visual_line.position.x > 0.1:
+			visual_line.rotation = 0
+		else:
+			visual_line.rotation = PI
 
 
 func _on_body_entered(_body: Node2D) -> void:
