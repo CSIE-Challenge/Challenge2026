ea56222370146517f48ccfda506d3b193470c9b0
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2026-07-05 23:46:33 +0800

    chore: dynamically generate inspector properties from enum

--- Scripts/audio.gd
+++ Scripts/audio.gd
@@ -1,20 +1,10 @@
+@tool
 extends Node
 
 enum SFX { JUMP, BUTTON_PRESS, ENERGY_COLLECTED }
 enum BGM { MENU, GAMEPLAY, HIDDEN_GAME, RESULT }
 
 @export var sfx_pool_size := 8
-
-@export_group("BGM Tracks")
-@export var menu_bgm_tracks: Array[AudioStream] = []
-@export var gameplay_bgm_tracks: Array[AudioStream] = []
-@export var hidden_game_bgm_tracks: Array[AudioStream] = []
-@export var result_bgm_tracks: Array[AudioStream] = []
-
-@export_group("Sound Effects")
-@export var jump: AudioStream
-@export var button_press: AudioStream
-@export var energy_collected: AudioStream
 
 var _sfx_players: Array[AudioStreamPlayer] = []
 var _bgm_playlist: Array[AudioStream] = []
@@ -35,20 +25,10 @@ func _ready() -> void:
 
 	bgm_player.finished.connect(_play_random_bgm_track)
 
-	_sfx_streams = {
-		SFX.JUMP: jump, SFX.BUTTON_PRESS: button_press, SFX.ENERGY_COLLECTED: energy_collected
-	}
-	_bgm_playlists = {
-		BGM.MENU: menu_bgm_tracks,
-		BGM.GAMEPLAY: gameplay_bgm_tracks,
-		BGM.HIDDEN_GAME: hidden_game_bgm_tracks,
-		BGM.RESULT: result_bgm_tracks,
-	}
-
 
 func set_bgm(bgm: BGM) -> void:
 	bgm_player.stop()
-	_bgm_playlist = _bgm_playlists.get(bgm, [])
+	_bgm_playlist = _bgm_playlists.get(bgm, []) as Array[AudioStream]
 	_last_track_index = -1
 	_play_random_bgm_track()
 	print("playing BGM.%s playlist" % BGM.keys()[bgm])
@@ -93,3 +73,62 @@ func _play_random_bgm_track() -> void:
 		_last_track_index = index
 	bgm_player.stream = _bgm_playlist[_last_track_index]
 	bgm_player.play()
+
+
+func _get_property_list() -> Array[Dictionary]:
+	var properties: Array[Dictionary] = []
+
+	properties.append({"name": "BGM Tracks", "type": TYPE_NIL, "usage": PROPERTY_USAGE_GROUP})
+
+	for bgm_type in BGM.keys():
+		properties.append(
+			{
+				"name": "%s_tracks" % bgm_type.to_lower(),
+				"type": TYPE_ARRAY,
+				"hint": PROPERTY_HINT_TYPE_STRING,
+				"hint_string": "24/17:AudioStream"
+			}
+		)
+
+	properties.append({"name": "Sound Effects", "type": TYPE_NIL, "usage": PROPERTY_USAGE_GROUP})
+
+	for sfx_name in SFX.keys():
+		properties.append(
+			{
+				"name": "%s_sfx" % sfx_name.to_lower(),
+				"type": TYPE_OBJECT,
+				"hint": PROPERTY_HINT_RESOURCE_TYPE,
+				"hint_string": "AudioStream",
+				"class_name": &"AudioStream"
+			}
+		)
+
+	return properties
+
+
+func _get(property):
+	if property.ends_with("_tracks"):
+		var bgm_type = property.left(-7)
+		var bgm_key = BGM.get(bgm_type.to_upper())
+		return _bgm_playlists.get(bgm_key, [])
+	elif property.ends_with("_sfx"):
+		var sfx_name = property.left(-4)
+		var sfx_key = SFX.get(sfx_name.to_upper(), [])
+		return _sfx_streams.get(sfx_key)
+	return null
+
+
+func _set(property, value):
+	if property.ends_with("_tracks"):
+		var bgm_type = property.left(-7)
+		var bgm_key = BGM.get(bgm_type.to_upper())
+		if not _bgm_playlists.has(bgm_key):
+			_bgm_playlists[bgm_key] = [] as Array[AudioStream]
+		_bgm_playlists[bgm_key].assign(value)
+		return true
+	elif property.ends_with("_sfx"):
+		var sfx_name = property.left(-4)
+		var sfx_key = SFX.get(sfx_name.to_upper())
+		_sfx_streams.set(sfx_key, value)
+		return true
+	return false
