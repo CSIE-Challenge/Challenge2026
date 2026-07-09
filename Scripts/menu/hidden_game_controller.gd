caf9eef7e20ff853f82f983471c2bf94eb2e4edf
WgLy <102111george@gmail.com>
2026-07-09 22:47:11 +0800

    fix: maintain hidden game path

--- Scripts/menu/hidden_game_controller.gd
+++ Scripts/menu/hidden_game_controller.gd
@@ -148,7 +148,7 @@ func _run_phase_1() -> bool:
 			await Dialogue.dialogue_finished
 		else:
 			await get_tree().create_timer(1.0).timeout
-		SceneTransition.transition_to_distortion("res://Scenes/menu/hidden_game.tscn")
+		SceneTransition.transition_to_distortion(HIDDEN_SCENE_PATH)
 		return true
 
 	return false
