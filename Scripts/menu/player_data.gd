e94907b93282962742934419d1cbaca37a315f3d
chilfox <chilfox06@gmail.com>
2026-07-14 18:16:28 +0800

    feat: Add black forest skin

--- Scripts/menu/player_data.gd
+++ Scripts/menu/player_data.gd
@@ -29,7 +29,8 @@ const REDEEM_CODES = {
 	"cc2f55d090a86899aeff8915f84e4dda53cdc45cfc3b2c1ec20d90d183fa3b69": "claude_skin",
 	"d031c4f465df0276f570a84d981a34e946f25e411f6e414d0416094802c38448": "perry_skin",
 	"b8ecc695944657ff55e46cf5799950dd4f97e7270fc83f9ffe82d2b5a4122c47": "red_black_tree_skin",
-	"0d4bb7907d956210fe58d62227286b38c857751f563aa9b822ff96225925bc52": "steel_brain_skin"
+	"0d4bb7907d956210fe58d62227286b38c857751f563aa9b822ff96225925bc52": "steel_brain_skin",
+	"1d718f314a4484758db49181edccb4e45245724e9ee38dfa72f9b7f8ac470d02": "three_body_coconut_skin"
 }
 
 var entered_codes: Array = []
